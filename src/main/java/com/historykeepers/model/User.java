0784fdeab928c96c52de46aa080af2704d7df287
Aricas77 <carineftali@gmail.com>
2025-12-11 23:53:07 -0600

    documentacion

--- src/main/java/com/historykeepers/model/User.java
+++ src/main/java/com/historykeepers/model/User.java
@@ -2,29 +2,31 @@ package com.historykeepers.model;
 
 import jakarta.persistence.*;
 
-@Entity
-@Table(name = "users")
+@Entity//Indica que esta clase es una entidad que se mapea a una tabla en la base de datos
+@Table(name = "users")//Definimos el nombre de la tabla en la base de datos
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    
+    @Column(unique = true, nullable = false)//Definimos que esta columna es unica y no puede ser nula
     private String username;
 
-    @Column(nullable = false)
+    @Column(nullable = false)//Definimos que esta columna no puede ser nula
     private String password;
 
-    @Column(name = "correo", nullable = false, unique = true)
+    @Column(name = "correo", nullable = false, unique = true)//Definimos el nombre de la columna en la tabla
     private String correo;
 
-    @Column(name = "nombre_completo", nullable = false)
+    @Column(name = "nombre_completo", nullable = false)//Definimos el nombre de la columna en la tabla
     private String nombreCompleto;
 
     public User() {
     }
 
+    //Constructores de la clase User
     public User(String username, String password) {
         this.username = username;
         this.password = password;
@@ -36,7 +38,7 @@ public class User {
         this.correo = correo;
         this.nombreCompleto = nombreCompleto;
     }
-
+    //Getters y Setters de nuestra clase User
     public Long getId() {
         return id;
     }
